d78263f96d0589cdcc1525b1ad82683b561ea01c
Pantasun <190232369@qq.com>
2017-09-12 22:17:46 +0800

    submit

---
+++ myblogproject_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\wamp64\www\myblogproject_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myblogproject_env/Scripts/markdown_py.bat
@@ -0,0 +1,1 @@
+@"d:\wamp64\www\myblogproject_env\scripts\python.exe" "d:\wamp64\www\myblogproject_env\Scripts\markdown_py" %*
